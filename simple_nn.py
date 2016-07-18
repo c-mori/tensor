5bab81c203afe2b14eb039216ad2567e02efbd54
c-mori <chris@northwall-llc.com>
2016-07-18 15:29:27 -0400

    continue exploration

--- simple_nn.py
+++ simple_nn.py
@@ -8,8 +8,8 @@ def rmse(predictions, actual):
     sse = np.sum(np.square(predictions - actual))
     return np.sqrt(sse / float(actual.shape[0]))
 
-n = 3
-hidden = n * 3
+n = 2
+hidden = n * 5
 
 # Generate training data and outputs
 train_x = np.random.normal(size=(20000, n))
@@ -26,7 +26,7 @@ train_x = train_x.astype(np.float32)
 
 batch_size = 125
 keep_percentage = .5
-learn_rate = 5e-4
+learn_rate = 3.5e-4
 lambda_ = 0
 
 num_labels = train_y.shape[1]
@@ -35,24 +35,23 @@ graph = tf.Graph()
 with graph.as_default():
     # Input data. For the training data, we use a placeholder that will be fed
     # at run time with a training minibatch.
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, n))
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(None, n), name='X')
     tf_train_labels = tf.placeholder(
-        tf.float32, shape=(batch_size, num_labels))
+        tf.float32, shape=(None, num_labels), name='y')
     tf_keep_prob = tf.placeholder(tf.float32, name='keep_rate')
-    dropout_keep_prob = tf.placeholder(tf.float32)
     tf_tr_dataset = tf.constant(train_x)
 
     # Variables.
-    w0 = tf.Variable(tf.truncated_normal([n, hidden]))
-    b0 = tf.Variable(tf.zeros([hidden]))
-    w1 = tf.Variable(tf.truncated_normal([hidden, num_labels]))
-    b1 = tf.Variable(tf.zeros([num_labels]))
+    w0 = tf.Variable(tf.truncated_normal([n, hidden]), name='w0')
+    b0 = tf.Variable(tf.zeros([hidden]), name='b0')
+    w1 = tf.Variable(tf.truncated_normal([hidden, num_labels]), name='w1')
+    b1 = tf.Variable(tf.zeros([num_labels]), name='b1')
 
     # Training computation.
     logits = tf.nn.relu(tf.matmul(tf_train_dataset, w0) + b0)
     logits = tf.nn.dropout(logits, tf_keep_prob)
     logits = tf.matmul(logits, w1) + b1
-    loss = (tf.reduce_sum(tf.square(logits - tf_train_labels)) / batch_size +
+    loss = (tf.reduce_sum(tf.square(logits - tf_train_labels)) +
             lambda_ * (tf.nn.l2_loss(w0) + tf.nn.l2_loss(w1)))
 
     # Optimizer.
@@ -60,11 +59,12 @@ with graph.as_default():
 
     # Predictions
     train_prediction = logits
+
     tr_preds = tf.nn.relu(tf.matmul(tf_tr_dataset, w0) + b0)
-    tr_preds = tf.nn.dropout(tr_preds, 1.0)
+    tr_preds = tf.nn.dropout(tr_preds, tf_keep_prob)
     tr_preds = tf.matmul(tr_preds, w1) + b1
 
-num_steps = 10001
+num_steps = 125001
 
 with tf.Session(graph=graph) as session:
     tf.initialize_all_variables().run()
@@ -85,13 +85,16 @@ with tf.Session(graph=graph) as session:
             tf_keep_prob: keep_percentage}
         _, l, predictions = session.run(
             [optimizer, loss, train_prediction], feed_dict=feed_dict)
-        if (step % 1000 == 0):
+        if (step % 2000 == 0):
             print("Minibatch loss at step %d: %f" % (step, l))
             print("Minibatch RMSE: %.5f" % rmse(predictions, batch_labels))
-            print("Train set RMSE: %.5f" % rmse(tr_preds.eval(), train_y))
-
+            # print("Train set RMSE: %.5f" % rmse(tr_preds.eval(), train_y))
     print("")
-    preds = tr_preds.eval()
+    feed_dict = {
+        tf_train_dataset: train_x,
+        tf_keep_prob: 1.0}
+    preds = logits.eval(feed_dict=feed_dict)
+    print preds
 
 print("actual y values:")
 print(train_y)
